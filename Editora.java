b9d67f69c6af036be76c03709a57b0d4ba85e309
João Pedro <85250950+Kiro2001@users.noreply.github.com>
2022-08-27 22:44:20 -0300

    Primeira versão da classe Editora

--- Editora.java
+++ Editora.java
@@ -1,12 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author 07339943188
- */
+import java.util.ArrayList;
+
+
 public class Editora {
+    private String nome,paisOrigem,dataCriacao;
+    private int qtdLivrosPublicados;
+    private ArrayList<Livro> livros= new ArrayList<>();
+    
+    public Editora() {
+    }
+
+    public Editora(String nome, String paisOrigem, String dataCriacao, int qtdLivrosPublicados) {
+        this.nome = nome;
+        this.paisOrigem = paisOrigem;
+        this.dataCriacao = dataCriacao;
+        this.qtdLivrosPublicados = qtdLivrosPublicados;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
+    }
+
+    public String getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public int getQtdLivrosPublicados() {
+        return qtdLivrosPublicados;
+    }
+
+    public void setQtdLivrosPublicados(int qtdLivrosPublicados) {
+        this.qtdLivrosPublicados = qtdLivrosPublicados;
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+     public void publicouLivro(Livro livro){
+        livros.add(livro);
+    }
     
 }
