02d0b0c0b76534b9eea411d1ec5ff38d1af81761
gustavodezan <gustavol.dezan@gmail.com>
2022-08-31 10:54:10 -0300

    Kiro2001 (Funcionario) -> main

--- biblioteca/Funcionario.java
+++ biblioteca/Funcionario.java
@@ -1,5 +1,34 @@
 package biblioteca;
 
-public class Funcionario {
+public class Funcionario extends Usuario{
+    private String cargo,setorResponsavel;
+
+    Funcionario(String nome, String senha, int id,String cargo, String setorResponsavel) {
+        super(nome,senha,id);
+        this.cargo = cargo;
+        this.setorResponsavel = setorResponsavel;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetorResponsavel() {
+        return setorResponsavel;
+    }
+
+    public void setSetorResponsavel(String setorResponsavel) {
+        this.setorResponsavel = setorResponsavel;
+    }
+    public void adicionarLivro(String nome, String dataDePublicacao, String genero,int qtDePaginas,Estante estante){
+        Livro livro= new Livro(nome,dataDePublicacao,genero,qtDePaginas);
+        estante.adicionarLivro(livro);
+    }
+    public void fazerPedido(){}
     
+    public void cadastrarUsuario(){}
 }
