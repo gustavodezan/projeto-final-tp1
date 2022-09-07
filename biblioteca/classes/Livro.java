bea0806e1ad8c07d9d3470b15f20cd115f4522c0
João Pedro <85250950+Kiro2001@users.noreply.github.com>
2022-09-07 13:14:13 -0300

    Add files via upload

---
+++ biblioteca/classes/Livro.java
@@ -0,0 +1,55 @@
+package biblioteca.classes;
+
+public class Livro{
+    private String nome,dataDePublicacao,genero;
+    private int qtDePaginas;
+
+    public Livro() {
+    }
+
+    public Livro(String nome, String dataDePublicacao, String genero, 
+            int qtDePaginas) {
+        this.nome = nome;
+        this.dataDePublicacao = dataDePublicacao;
+        this.genero = genero;
+        this.qtDePaginas = qtDePaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataDePublicacao() {
+        return dataDePublicacao;
+    }
+
+    public void setDataDePublicacao(String dataDePublicacao) {
+        this.dataDePublicacao = dataDePublicacao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getQtDePaginas() {
+        return qtDePaginas;
+    }
+
+    public void setQtDePaginas(int qtDePaginas) {
+        this.qtDePaginas = qtDePaginas;
+    }
+    
+    public String mostrarInformacoes(){
+        return "Nome: "+this.nome+"\nData de Publicação: "+this.dataDePublicacao
+                +"\nQuantidade de Paginas: "+this.qtDePaginas+"\nGênero: "
+                +this.genero;
+    }  
+}
