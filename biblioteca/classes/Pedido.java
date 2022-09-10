a05ca19d1e89adeb0752a959edecb87128b194c7
João Pedro <85250950+Kiro2001@users.noreply.github.com>
2022-09-10 19:35:06 -0300

    Update Pedido.java

--- biblioteca/classes/Pedido.java
+++ biblioteca/classes/Pedido.java
@@ -5,6 +5,53 @@
 package biblioteca.classes;
 
 
+import java.util.GregorianCalendar;
+
+
 public class Pedido {
+    private int id, numLivros;
+    private GregorianCalendar data;
+
+    public Pedido() {
+        data = new GregorianCalendar();
+    }
     
+    public Pedido(int id, int numLivros) {
+        this.id = id;
+        this.numLivros = numLivros;
+        this.data = new GregorianCalendar();
+    }
+    
+    public boolean verificarValidade(int limite){
+        if (numLivros <= limite) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumLivros() {
+        return numLivros;
+    }
+
+    public void setNumLivros(int numLivros) {
+        this.numLivros = numLivros;
+    }
+
+    public GregorianCalendar getData() {
+        return data;
+    }
+
+    public void setData(GregorianCalendar data) {
+        this.data = data;
+    }
 }
