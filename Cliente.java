ca28f15b1d94b539981a7438d487c30bd1e77a01
João Pedro Felicio Pereira <72157587+joaocabeca2@users.noreply.github.com>
2022-09-10 14:28:35 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author 07339943188
- */
-public class Cliente {
-    
+package biblioteca;
+import java.util.HashMap;
+import java.util.ArrayList;
+public class Cliente extends Usuario {
+    private String historicoLivros,dataInscricao,nivelLeitor;
+
+    public Cliente(){}
+    public Cliente(String nome,String senha,int id,
+    String historicoLivros,String dataInscricao,String nivelLeitor){
+        super(nome,senha,id);
+        this.historicoLivros = historicoLivros;
+        this.dataInscricao = dataInscricao;
+        this.nivelLeitor = nivelLeitor;
+    }
+    public String getHistoricoLivros(){
+        return this.historicoLivros;
+    }
+    public void setHistoricoLivros(String historicoLivros){
+        this.historicoLivros = historicoLivros;
+    }
+    public String getDataIncricao(){
+        return this.dataInscricao;
+    }
+    public void setDataInscricao(String dataInscricao){
+        this.dataInscricao = dataInscricao;
+    }
+    public String getNivelLeitor(){
+        return this.nivelLeitor;
+    }
+    public void setNivelLeitor(String nivelLeitor){
+        this.nivelLeitor = nivelLeitor;
+    }
+    //public boolean solicitarEmprestimo(String nomeLivro,String genero){};
+    public boolean verificarLivro(String nomeLivro,String genero,HashMap<String,ArrayList<Livro>> mapa){
+        ArrayList<Livro>  listaLivros = mapa.get(genero);
+        if(listaLivros != null){
+            for(Livro livro:listaLivros){
+                if(livro.getNome().equals(nomeLivro)){
+                    return true;
+                }
+            }
+            return false;
+        }else{
+            return false;
+        }
+    }
+    public void devolverLivro(String nomeLivro,String genero){};
+    public String listarLivros(String genero,HashMap<String,ArrayList<Livro>> mapa){
+        for(String generoEstante:mapa.keySet()){
+            if(generoEstante.equals(genero)){
+                return mapa.get(genero).toString();
+            }
+        }
+        return null;
+    }
 }
